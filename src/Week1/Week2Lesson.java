af653616176e9492b8f4d591ad983602b69d2af1
Melisa Çevik <celissamevik@gmail.com>
2024-01-13 10:58:48 +0300

    adding week2 factorial problem

--- src/Week1/Week2Lesson.java
+++ src/Week1/Week2Lesson.java
@@ -8,6 +8,8 @@ public class Week2Lesson {
         // kullanıcıdan sürekli sayı isteyin ve faktöriyeli hesaplayınız.
         // 2.hafta
 
+        /*
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Faktoriyelini hesaplamak istediğiniz sayıyı girin: ");
 
@@ -30,7 +32,47 @@ public class Week2Lesson {
             i = 1;
             counter++;
 
+
+
         }
+
+         */
+
+        //Bir öğrencinin sinav notlarını giriş olarak alın ve bu notların ortalamasını alınız.
+        /*
+        Scanner scan = new Scanner(System.in);
+        int i = 0 ;
+        double toplam = 0 ;
+
+        while ( i < 3) {
+            System.out.println("Lütfen not giriniz: ");
+            double not = scan.nextDouble();
+            toplam += not ;
+            i++;
+        }
+
+        System.out.println("Ortalama " + (toplam / 3));
+
+         */
+
+        // Kaç dersin olduğunu bilmiyorsak ;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ders sayısını giriniz: ");
+        int dersSayisi = scan.nextInt();
+
+        int toplamNot = 0 ;
+        int sayac = 0;
+
+        while ( sayac < dersSayisi) {
+            System.out.println((sayac + 1 )+ "." + "notunuz: ");
+            int not = scan.nextInt();
+            toplamNot += not;
+            sayac++;
+        }
+
+        double ortalama = (double) toplamNot / dersSayisi;
+        System.out.println("Not ortalaması: " + ortalama);
 
     }
 }
